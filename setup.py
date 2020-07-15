1feb147fc8a51488566315d383a24b6347b0bc69
Matthew Ruse <matthew.ruse@gmail.com>
2020-07-15 15:42:51 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='anticaptcha-python',
+    version='',
+    packages=['anticaptchaofficial'],
+    url='',
+    license='',
+    author='matth',
+    author_email='',
+    description=''
+)
